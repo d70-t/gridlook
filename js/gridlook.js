87f76d0c605b90aa55ce4891825ab797a9d16eb3
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2021-11-02 18:04:25 +0100

    do colormapping in shader

--- js/gridlook.js
+++ js/gridlook.js
@@ -35,6 +35,32 @@ async function grid2buffer(grid) {
     return verts;
 }
 
+async function data2value_buffer(data) {
+    data = await data;
+    const ncells = data.shape[0];
+    const plotdata = await data.data
+
+    let data_min = Number.POSITIVE_INFINITY;
+    let data_max = Number.NEGATIVE_INFINITY;
+    for (var i = 0; i < ncells; i++) {
+        const v = plotdata[i];
+        if (v < data_min) data_min = v;
+        if (v > data_max) data_max = v;
+    }
+
+    const norm = d => (d - data_min) / (data_max - data_min);
+
+    let data_values = new Float32Array(ncells * 3);
+
+    for (var i = 0; i < ncells; i++) {
+        const v = norm(plotdata[i]);
+        data_values[3 * i + 0] = v;
+        data_values[3 * i + 1] = v;
+        data_values[3 * i + 2] = v;
+    }
+    return data_values;
+}
+
 async function data2color_buffer(data, colormap) {
     data = await data;
     const ncells = data.shape[0];
@@ -87,4 +113,50 @@ function snapshot(canvas_id, render) {
         // delete the internal blob reference, to let the browser clear memory from it
         URL.revokeObjectURL(link.href);
     }, 'image/png');
+}
+
+function make_colormap_material() {
+    const vertexShader = `
+        attribute float data_value;
+
+        varying float v_value;
+
+        void main() {
+          v_value = data_value;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+        }
+        `;
+
+    const fragmentShader = `
+        vec3 viridis(float t) {
+
+            const vec3 c0 = vec3(0.2777273272234177, 0.005407344544966578, 0.3340998053353061);
+            const vec3 c1 = vec3(0.1050930431085774, 1.404613529898575, 1.384590162594685);
+            const vec3 c2 = vec3(-0.3308618287255563, 0.214847559468213, 0.09509516302823659);
+            const vec3 c3 = vec3(-4.634230498983486, -5.799100973351585, -19.33244095627987);
+            const vec3 c4 = vec3(6.228269936347081, 14.17993336680509, 56.69055260068105);
+            const vec3 c5 = vec3(4.776384997670288, -13.74514537774601, -65.35303263337234);
+            const vec3 c6 = vec3(-5.435455855934631, 4.645852612178535, 26.3124352495832);
+
+            return c0+t*(c1+t*(c2+t*(c3+t*(c4+t*(c5+t*c6)))));
+
+        }
+
+        varying float v_value;
+
+        void main() {
+          gl_FragColor.a = 1.0;
+          gl_FragColor.rgb = viridis(v_value);
+        }
+        `;
+
+    const material = new THREE.ShaderMaterial({
+        uniforms: {
+
+        },
+
+        vertexShader,
+        fragmentShader
+    });
+    return material;
 }
