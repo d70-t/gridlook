262ae97c9b60f17764ee09bd2ebbe39d509587eb
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2022-04-05 15:53:35 +0200

    hide navigation for now

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
+  <!--
   <header>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
   </header>
-
+  -->
   <RouterView />
 </template>
 
