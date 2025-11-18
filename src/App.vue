77f0311fc1cc8e0f1037948b645c6889e52a7b85
Andrej Fast <andrej.fast@dkrz.de>
2025-11-18 14:43:32 +0100

    added shuffle codec

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 //import { RouterLink, RouterView } from 'vue-router'
 import HashGlobeView from "@/views/HashGlobeView.vue";
+import "./components/utils/ShuffleCodec";
 </script>
 
 <template>
