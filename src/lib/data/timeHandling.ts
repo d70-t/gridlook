a0e313596943b4c0f2a71a9e275341f9a87ee2a7
Andrej Fast <andrej.fast@dkrz.de>
2026-01-13 13:47:31 +0100

    added support for nanoseconds

--- src/lib/data/timeHandling.ts
+++ src/lib/data/timeHandling.ts
@@ -5,14 +5,21 @@ import * as zarr from "zarrita";
 dayjs.extend(utc);
 
 export function decodeTime(value: number, attrs: zarr.Attributes) {
-  const unitsRegEx = /([a-zA-Z]+) since (.+)$/;
+  const unitsRegEx = /([a-zA-Z]+) since (.+)$/i;
   const units: string = attrs.units as string;
   const regExMatch = units.match(unitsRegEx);
   if (!regExMatch) {
     throw new Error("time units not recognized");
   }
-  const [, interval, refdate] = regExMatch;
+  let interval = regExMatch[1];
+  const refdate = regExMatch[2];
+
   const ref = dayjs.utc(refdate);
+  // dayjs does not support nanoseconds, so convert to milliseconds
+  if (["nanosecond", "nanoseconds", "ns"].includes(interval.toLowerCase())) {
+    value = value / 1e6;
+    interval = "millisecond";
+  }
   const timepoint = ref.add(value, interval as dayjs.ManipulateType);
   return timepoint;
 }
