04b0bc9bbd28bde2015335a127aa6b249b20095a
Andrej Fast <andrejfast@gmail.com>
2026-04-22 20:50:32 +0200

    chore(lib): fixed linter issues

--- src/lib/data/bitround.ts
+++ src/lib/data/bitround.ts
@@ -1,7 +1,4 @@
-import type { Float32, Float64 } from "zarrita";
-
 interface BitroundCodecConfig {
-  /** Number of bytes per element (default: 1) */
   keepbits: number;
 }
 /**
@@ -9,24 +6,18 @@ interface BitroundCodecConfig {
  * For some magical reason zarrita has dropped bitround for Zarr V2?
  * We introduce it back again!
  */
-export class BitroundCodec<D extends Float64 | Float32> {
+export class BitroundCodec {
   kind = "array_to_array" as const;
 
-  constructor(
-    configuration: BitroundCodecConfig = { keepbits: 8 },
-    _meta: { dataType: D }
-  ) {
+  constructor(configuration: BitroundCodecConfig = { keepbits: 8 }) {
     if (configuration.keepbits < 0) {
       throw new Error("keepbits must be zero or positive");
     }
   }
 
-  static fromConfig(config: unknown, meta: unknown) {
+  static fromConfig(config: unknown) {
     const validated = (config as BitroundCodecConfig) ?? { keepbits: 8 };
-    return new BitroundCodec(
-      validated,
-      meta as { dataType: Float32 | Float64 }
-    );
+    return new BitroundCodec(validated);
   }
 
   encode(): never {
